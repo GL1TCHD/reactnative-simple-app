323a57d4d3dc4a28c0ec4042e45b657586e3cebc
murtaja <albasti1100@gmail.com>
2026-04-29 16:02:08 +0300

    feat: init

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,6 +1,8 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {  SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+ import {styled} from 'nativewind'
+const SafeAreaView = styled(RNSafeAreaView);
 
 const settings = () => {
   return (
